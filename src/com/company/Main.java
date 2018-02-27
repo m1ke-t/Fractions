934f11554bfface2d353b450d2610b1ad118b787
m1ke-t <miseven95@gmail.com>
2018-02-27 19:58:47 +1000

    Init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,7 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
     static final Logger log = LoggerFactory.getLogger(Main.class.getName());
-
-
+    
 
     public static void main(String[] args) {
 
